ec0bc06f169d8d35d362559242c9203e87333e9d
Nitish Madhukar <nitishmadhukar@gmail.com>
2017-03-08 04:36:27 -0500

    update config for assets

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Ymdb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
-    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
   end
 end
